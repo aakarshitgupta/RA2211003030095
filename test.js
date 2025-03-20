0b18e48efe8192be6777de0a957cb6651e3e408d
Akarshit <aakarshitgupta307@gmail.com>
2025-03-20 18:58:48 +0530

    registeration done

--- test.js
+++ test.js
@@ -4,11 +4,13 @@ const { response } = require("express");
 const url = "http://20.244.56.144/test/register";
 const about = {
     companyName : "goMart",
-    ownerName : "Rahul",
+    ownerName : "Akarshit Gupta",
     rollno  : "RA2211003030095",
-    ownerEmail : "rahul@abc.edu",
+    ownerEmail : "ag8251@srmist.edu.in",
     accessCode : "SUfGJv"
 }
 axios.post(url,about, {headers: {"Content-Type":"application/json"}}).then(response => {
     console.log( response.data);
-}).catch(console.error(error.message));
+}).catch((error)=>{
+    console.error(error.response);
+});
